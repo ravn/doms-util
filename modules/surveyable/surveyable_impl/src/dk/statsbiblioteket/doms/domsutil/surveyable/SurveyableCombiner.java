b18d3f338d4ea8416398d3e20b705b391b0e614b
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2010-03-25 13:53:16 +0000

    Surveillance interfaces now generated from WSDLs, and SOAP and REST webservice implemented to work on the Surveyable singleton.
    Currently the module generates a WAR-file in dist, but this is probably unwanted behaviour since we want to use it from inside other WAR-files instead.

---
+++ modules/surveyable/surveyable_impl/src/dk/statsbiblioteket/doms/domsutil/surveyable/SurveyableCombiner.java
@@ -0,0 +1,177 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * The DOMS project.
+ * Copyright (C) 2007-2010  The State and University Library
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package dk.statsbiblioteket.doms.domsutil.surveyable;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import dk.statsbiblioteket.doms.webservices.ConfigCollection;
+import dk.statsbiblioteket.util.qa.QAInfo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Properties;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Combines several Surveyable classes into one.
+ * The classes to combine are controlled by the parameter
+ * dk.statsbiblioteket.doms.domsutil.surveyable.surveyables
+ * which is list ;-separated list of factories that produce surveyables.
+ */
+@QAInfo(author = "kfc",
+        reviewers = "jrg",
+        state = QAInfo.State.QA_NEEDED,
+        level = QAInfo.Level.NORMAL)
+public class SurveyableCombiner implements Surveyable {
+    /** List of surveyables to combine into one. */
+    private List<Surveyable> surveyables;
+
+    /** Prefix for configuration parameters. */
+    private static final String CONFIGURATION_PACKAGE_NAME
+            = "dk.statsbiblioteket.doms.domsutil.surveyable";
+
+    /** Parameter for classes to use as surveyables. Separated by ;. */
+    private static final String CONFIGURATION_SURVEYABLES_PARAMETER =
+            CONFIGURATION_PACKAGE_NAME + ".surveyables";
+
+    /** Log for this class. */
+    private Log log = LogFactory.getLog(getClass());
+
+    /**
+     * Read configuration for classes to survey, and initialise the classes, to
+     * expose them as one surveyable.
+     */
+    public SurveyableCombiner() {
+        log.trace("Enter SurveyableCombiner()");
+        Properties config = ConfigCollection.getProperties();
+        String classes = config
+                .getProperty(CONFIGURATION_SURVEYABLES_PARAMETER);
+        if (classes != null && classes.trim().length() != 0) {
+            surveyables = new ArrayList<Surveyable>();
+            for (String classname : classes.split(";")) {
+                log.debug("Initialising class '" + classname
+                        + "' for surveillance");
+                Surveyable surveyable;
+                try {
+                    surveyable = SurveyableFactory.createSurveyable(classname);
+                } catch (Exception e) {
+                    log.error("Unable to initialise class for surveillance: '"
+                            + classname + "'", e);
+                    surveyable = new NoSurveyable(classname);
+                }
+                surveyables.add(surveyable);
+            }
+        } else {
+            log.warn("No classes specified for surveillance.");
+            surveyables = Collections
+                    .singletonList((Surveyable) new NoSurveyable());
+        }
+    }
+
+    /**
+     * Get the combined list of all status messages newer than the given time.
+     * An application should use the newest timestamp in the given messages
+     * from the last call as input to this method next time it calls it, to
+     * ensure not losing messages.
+     *
+     * Note, the name in the returned status is the name from the first
+     * surveyable in the list of combined status objects.
+     *
+     * @param time Only get messages strictly newer than this timestamp. The
+     *             timestamp is measured in milliseconds since
+     *             1970-01-01 00:00:00.000Z.
+     * @return List of status messages. May be empty, but never null. If no
+     *         surveyables are in the list to combine, will return a list of
+     *         just one message, which is a message about not being properly
+     *         initialised.
+     */
+    public Status getStatusSince(long time) {
+        log.trace("Enter getStatusSince(" + time + ")");
+
+        StatusMessage statusMessage = new StatusMessage();
+        Status status = new Status();
+
+        if (surveyables == null || surveyables.size() == 0) {
+            statusMessage.setLogMessage(false);
+            statusMessage.setTime(System.currentTimeMillis());
+            statusMessage.setSeverity(Severity.RED);
+
+            status.setName("Survey configuration error");
+            status.getMessages().add(statusMessage);
+
+            return status;
+        }
+        SortedSet<StatusMessage> messages = new TreeSet<StatusMessage>(
+                new StatusMessageComparator());
+        for (Surveyable surveyable : surveyables) {
+            messages.addAll(surveyable.getStatusSince(time).getMessages());
+        }
+        status.setName(surveyables.get(0).getStatus().getName());
+        status.getMessages().addAll(messages);
+        return status;
+    }
+
+    /**
+     * Get all status messages. This behaves exactly like
+     * getMessagesSince(0L).
+     *
+     * @return List of status messages. May be empty, but never null.
+     *
+     * @see #getStatusSince(long)
+     */
+    public Status getStatus() {
+        log.trace("Enter getStatus()");
+        return getStatusSince(0L);
+    }
+
+    /** Comparator that orders status messages with regard to date. */
+    private static class StatusMessageComparator
+            implements Comparator<StatusMessage> {
+        /**
+         * Compares its two arguments for order.  Returns a negative integer,
+         * zero, or a positive integer as the first argument is less than, equal
+         * to, or greater than the second.<p>
+         *
+         * This comparator is inconsistent with equals.
+         *
+         * @param o1 the first object to be compared.
+         * @param o2 the second object to be compared.
+         * @return a negative integer, zero, or a positive integer as the
+         *         first argument is less than, equal to, or greater than the
+         *         second.
+         */
+        public int compare(StatusMessage o1, StatusMessage o2) {
+            return (o1.getTime() == o2.getTime() ? 0
+                    : (o1.getTime() < o2.getTime() ? -1 : 1));
+        }
+    }
+}
---
+++ modules/surveyable/surveyable_impl/src/dk/statsbiblioteket/doms/domsutil/surveyable/SurveyableWebservice.java
@@ -0,0 +1,98 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * The DOMS project.
+ * Copyright (C) 2007-2010  The State and University Library
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package dk.statsbiblioteket.doms.domsutil.surveyable;
+
+import dk.statsbiblioteket.util.qa.QAInfo;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
+
+/** REST and SOAP webservice that exposes a surveyable singleton. */
+@WebService
+@QAInfo(level = QAInfo.Level.NORMAL,
+        state = QAInfo.State.QA_NEEDED,
+        author = "jrg",
+        reviewers = {"kfc"})
+@Path("/surveillance/")
+public class SurveyableWebservice implements Surveyable {
+    /**
+     * Get all status messages newer than the given time.
+     * An application should use the newest timestamp in the given messages
+     * from the last call as input to this method next time it calls it, to
+     * ensure not losing messages.
+     *
+     * @param time Only get messages strictly newer than this timestamp. The
+     *             timestamp is measured in milliseconds since 1970-01-01 00:00:00.000Z.
+     * @return List of status messages. May be empty, but never null.
+     */
+    @WebMethod
+    @WebResult(targetNamespace = "")
+    @RequestWrapper(localName = "getStatusSince",
+                    targetNamespace = "http://surveyable.domsutil.doms.statsbiblioteket.dk/",
+                    className = "dk.statsbiblioteket.doms.domsutil.surveyable.GetStatusSince")
+    @ResponseWrapper(localName = "getStatusSinceResponse",
+                     targetNamespace = "http://surveyable.domsutil.doms.statsbiblioteket.dk/",
+                     className = "dk.statsbiblioteket.doms.domsutil.surveyable.GetStatusSinceResponse")
+    @GET
+    @Path("getStatusSince/{date}")
+    @Produces("application/xml")
+    public Status getStatusSince(
+            @WebParam(name = "arg0", targetNamespace = "") long time) {
+        return SurveyableFactory.getSurveyable().getStatusSince(time);
+    }
+
+    /**
+     * Get all status messages. This behaves exactly like
+     * getMessagesSince(0L).
+     *
+     * @return List of status messages. May be empty, but never null.
+     *
+     * @see #getStatusSince(long)
+     */
+    @WebMethod
+    @WebResult(targetNamespace = "")
+    @RequestWrapper(localName = "getStatus",
+                    targetNamespace = "http://surveyable.domsutil.doms.statsbiblioteket.dk/",
+                    className = "dk.statsbiblioteket.doms.domsutil.surveyable.GetStatus")
+    @ResponseWrapper(localName = "getStatusResponse",
+                     targetNamespace = "http://surveyable.domsutil.doms.statsbiblioteket.dk/",
+                     className = "dk.statsbiblioteket.doms.domsutil.surveyable.GetStatusResponse")
+    @GET
+    @Path("getStatus")
+    @Produces("application/xml")
+    public Status getStatus() {
+        return SurveyableFactory.getSurveyable().getStatus();
+    }
+}
