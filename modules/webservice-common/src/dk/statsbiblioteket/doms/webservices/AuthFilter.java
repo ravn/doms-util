1c02d65f776d85a4239f2d5d72177bd1d84f9e99
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2010-05-27 20:04:32 +0000

    Suggested fix to auth trouble in testbed. Needs informal review before domsutil release

--- modules/webservice-common/src/dk/statsbiblioteket/doms/webservices/AuthFilter.java
+++ modules/webservice-common/src/dk/statsbiblioteket/doms/webservices/AuthFilter.java
@@ -17,25 +17,26 @@ public class AuthFilter implements Filter {
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
         //if Authenticate header already set
         if (request instanceof HttpServletRequest) {
             HttpServletRequest httpServletRequest = (HttpServletRequest) request;
             try {
-                Credentials creds = ExtractCredentials.extract(httpServletRequest);
-                request.setAttribute("Credentials",creds);
-                chain.doFilter(request,response);
+                if (!"wsdl".equals(httpServletRequest.getQueryString())) {
+                    Credentials creds = ExtractCredentials.extract(httpServletRequest);
+                    request.setAttribute("Credentials",creds);
+                }
             } catch (CredentialsException e) {        //else send request for authenticate
                 if (response instanceof HttpServletResponse) {
                     HttpServletResponse httpServletResponse = (HttpServletResponse) response;
                     httpServletResponse.addHeader("WWW-Authenticate","BASIC "+realm);
                     httpServletResponse.sendError(401);
+                    return;
                 }
             }
         }
+        chain.doFilter(request,response);
     }
 
     public void destroy() {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
