daeead56c72375e2ff8f98a33fc495457c789817
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2010-05-28 08:43:48 +0000

    Rolled back recognision of wsdl URLs. This would lead to a possibility for gaining access to any data, simply by appending "?wsdl" to the URL

--- modules/webservice-common/src/dk/statsbiblioteket/doms/webservices/AuthFilter.java
+++ modules/webservice-common/src/dk/statsbiblioteket/doms/webservices/AuthFilter.java
@@ -1,8 +1,40 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * The DOMS project.
+ * Copyright (C) 2007-2010  The State and University Library
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package dk.statsbiblioteket.doms.webservices;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.*;
 import java.io.IOException;
 
 public class AuthFilter implements Filter {
@@ -21,10 +53,8 @@ public class AuthFilter implements Filter {
         if (request instanceof HttpServletRequest) {
             HttpServletRequest httpServletRequest = (HttpServletRequest) request;
             try {
-                if (!"wsdl".equals(httpServletRequest.getQueryString())) {
-                    Credentials creds = ExtractCredentials.extract(httpServletRequest);
-                    request.setAttribute("Credentials",creds);
-                }
+                Credentials creds = ExtractCredentials.extract(httpServletRequest);
+                request.setAttribute("Credentials",creds);
             } catch (CredentialsException e) {        //else send request for authenticate
                 if (response instanceof HttpServletResponse) {
                     HttpServletResponse httpServletResponse = (HttpServletResponse) response;
