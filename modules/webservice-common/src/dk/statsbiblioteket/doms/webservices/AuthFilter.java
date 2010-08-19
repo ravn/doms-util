067d168a3da8d9b4b2cd0191b3deb94b013574c3
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2010-08-19 09:36:44 +0000

    Authfilter no longer asks for credentials if 1. the request is GET 2. Not path-params is given 3. the query string is ?wsdl

--- modules/webservice-common/src/dk/statsbiblioteket/doms/webservices/AuthFilter.java
+++ modules/webservice-common/src/dk/statsbiblioteket/doms/webservices/AuthFilter.java
@@ -52,15 +52,22 @@ public class AuthFilter implements Filter {
         //if Authenticate header already set
         if (request instanceof HttpServletRequest) {
             HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-            try {
-                Credentials creds = ExtractCredentials.extract(httpServletRequest);
-                request.setAttribute("Credentials",creds);
-            } catch (CredentialsException e) {        //else send request for authenticate
-                if (response instanceof HttpServletResponse) {
-                    HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-                    httpServletResponse.addHeader("WWW-Authenticate","BASIC "+realm);
-                    httpServletResponse.sendError(401);
-                    return;
+            if (httpServletRequest.getMethod().equals("GET")
+                && httpServletRequest.getPathInfo() == null
+                && httpServletRequest.getQueryString() == "?wsdl") {
+                //do not request credentials
+            } else{
+
+                try {
+                    Credentials creds = ExtractCredentials.extract(httpServletRequest);
+                    request.setAttribute("Credentials",creds);
+                } catch (CredentialsException e) {        //else send request for authenticate
+                    if (response instanceof HttpServletResponse) {
+                        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+                        httpServletResponse.addHeader("WWW-Authenticate","BASIC "+realm);
+                        httpServletResponse.sendError(401);
+                        return;
+                    }
                 }
             }
         }
